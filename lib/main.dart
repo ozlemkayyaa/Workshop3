ea75fc8ccfadcb587cd2f6d63ccc89419ca23c0d
Ozlem Kaya <126676960+ozlemkayyaa@users.noreply.github.com>
2023-11-01 14:53:45 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -33,34 +33,3 @@ void main() {
     ),
   );
 }
-
-/* class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: const Color.fromARGB(255, 80, 19, 126),
-        body: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Özlem Kaya',
-                style: TextStyle(fontSize: 30, color: Colors.white),
-              ),
-              Text(
-                'Tobeto - Mobil Geliştirici - 1B',
-                style: TextStyle(fontSize: 22, color: Colors.white),
-              ),
-              Text(
-                '31.10.2023',
-                style: TextStyle(fontSize: 18, color: Colors.white),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}*/
